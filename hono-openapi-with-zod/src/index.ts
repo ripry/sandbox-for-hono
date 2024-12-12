ad7539ffcd326746d5734d83f72fa948ee876835
Riku YAMAUCHI <ripry258@gmail.com>
2024-12-12 21:24:49 +0900

    Modify first route
    ref: https://hono.dev/examples/hono-openapi#hono-openapi

--- hono-openapi-with-zod/src/index.ts
+++ hono-openapi-with-zod/src/index.ts
@@ -1,16 +1,40 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { describeRoute } from 'hono-openapi'
+import { resolver, validator } from 'hono-openapi/zod'
+import { z } from 'zod'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const querySchema = z.object({
+  name: z.string().optional(),
 })
+const responseSchema = z.string()
+
+app.get(
+  '/',
+  describeRoute({
+    description: 'Say hello to the user',
+    responses: {
+      200: {
+        description: 'Successful response',
+        content: {
+          'text/plain': { schema: resolver(responseSchema) },
+        },
+      },
+    },
+  }),
+  validator('query', querySchema),
+  (c) => {
+    const { name } = c.req.valid('query')
+    return c.text(`Hello ${name ?? 'Hono'}!`)
+  },
+)
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
   fetch: app.fetch,
-  port
+  port,
 })
